3f0dbaf43ec69deeb9be518486e56f98a9be2ed6
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-16 01:48:59 -0300

    Update style

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -10,7 +10,7 @@ export const Container = styled.header`
     box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
 
     .img {
-        width: 2.8rem;
+        width: 2.6rem;
         height: auto;
     }
 `;
