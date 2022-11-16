27ef96d2e50ca51a9e7a4795ee925c54e54958fd
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-16 00:52:14 -0300

    Inclusão component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { Container } from "./style";
+import logoImg from '../../../public/logo.svg'
+
+export default function Header() {
+    return (
+        <Container>
+            <img src="../../../public/logo.svg" alt="" />
+        </Container>
+    );
+};
