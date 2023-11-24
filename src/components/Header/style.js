b560ecde85552a5e3017cc376b6b4a1576320f38
Paulo Marques <78260151+paulomarquesdev@users.noreply.github.com>
2023-11-24 02:12:30 -0300

    Update style.js

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -8,7 +8,7 @@ export const Container = styled.header`
     align-items: center;
     justify-content: center;
     /* box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25); */
-    border-bottom-style: 1px solid var(--border)
+    border-bottom: 1px solid var(--border)
 
     .img {
         width: 2.6rem;
