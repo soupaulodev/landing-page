fae2bbfe14d81ad03e9534b5f9d1d1b4d047d6f9
Paulo Marques <78260151+soupaulodev@users.noreply.github.com>
2024-03-17 20:51:53 -0300

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,4 +1,4 @@
-import { Container } from "./style";
+loimport { Container } from "./style";
 import Image from "next/image";
 import profileImg from '../../assets/image/logo.png';
 
@@ -8,7 +8,7 @@ export default function Profile() {
             <img src="https://avatars.githubusercontent.com/u/78260151?v=4" alt="Avatar do paulo marques" />
             <div>
                 <h1>Paulo Marques</h1>
-                <h2>Software Engineer</h2>
+                <h2>Web Developer</h2>
             </div>
         </Container>
     );
